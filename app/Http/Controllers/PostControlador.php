b387a179588d48ace5bffa2b7b274d8982a5c5aa
Eduardo Moura <oeduardomoura@gmail.com>
2019-10-24 13:07:20 -0300

    Download de arqivos

--- app/Http/Controllers/PostControlador.php
+++ app/Http/Controllers/PostControlador.php
@@ -47,6 +47,19 @@ class PostControlador extends Controller
 
     }
 
+    public function download()
+    {
+        $post = Post::find($id);
+        if(isset($post)){
+            $path = Storage::disk('public')->getDriver()->getAdapter()->applyPathPrefix($post->arquivo);
+            return response()->download($path);
+
+        }
+        return redirect('/');
+
+
+    }
+
     /**
      * Display the specified resource.
      *
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@
 Route::get('/', 'PostControlador@index');
 Route::post('/', 'PostControlador@store');
 Route::delete('/{id}', 'PostControlador@destroy');
+Route::get('/download/{id}', 'PostControlador@download');
 
